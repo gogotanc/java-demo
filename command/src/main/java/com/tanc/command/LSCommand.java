9a045393cd2246f12ba73bb0a77ac1b12d6dcedd
gogotanc <gogotanc@163.com>
2016-11-22 18:56:11 +0800

    java 调用 shell 命令

---
+++ command/src/main/java/com/tanc/command/LSCommand.java
@@ -0,0 +1,31 @@
+package com.tanc.command;
+
+import java.io.*;
+
+/**
+ * 执行 ls 命令，并返回结果
+ *
+ * @author tanc
+ */
+public class LSCommand {
+
+    public static void main(String[] args) {
+
+        String cmd = "ls -al";
+
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec(cmd);
+
+            BufferedInputStream in = new BufferedInputStream(process.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String content;
+
+            while ((content = reader.readLine()) != null) {
+                System.out.println(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
