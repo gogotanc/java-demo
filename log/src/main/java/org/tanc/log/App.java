601c40087b77a1599bb2dc75e2b61778ebbe2860
tanc <gogotanc@163.com>
2017-02-21 16:48:14 +0800

    slf4j + logback

---
+++ log/src/main/java/org/tanc/log/App.java
@@ -0,0 +1,13 @@
+package org.tanc.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.debug("Hello Log");
+    }
+}
