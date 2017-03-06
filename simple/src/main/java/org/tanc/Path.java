b8ecc79ba32d08d50462f869107b9b8d6313632d
tanc <gogotanc@163.com>
2017-03-06 18:37:58 +0800

    how to get tempdir path

--- simple/src/main/java/org/tanc/Path.java
+++ simple/src/main/java/org/tanc/Path.java
@@ -15,21 +15,29 @@ public class Path {
     private static Logger logger = LoggerFactory.getLogger(Path.class);
 
     public static void main(String[] args) throws IOException {
-        method1();
-        method2();
+        path01();
+        path02();
+        path03();
     }
 
-    public static void method1() {
+    private static void path01() {
         String path = System.getProperty("user.dir");
 
         logger.info("user.dir: {}", path);
     }
 
-    public static void method2() throws IOException {
+    private static void path02() throws IOException {
 
         File file = new File("");
 
         logger.info("标准路径: {}", file.getCanonicalPath());
         logger.info("绝对路径: {}", file.getAbsolutePath());
+    }
+
+    private static void path03() throws IOException {
+
+        String path = System.getProperty("java.io.tmpdir");
+
+        logger.info("临时路径: {}", path);
     }
 }
