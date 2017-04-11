25da0edb5a44c87a9cf21a40b3f29a660b28d86e
tanc <gogotanc@163.com>
2017-04-11 10:09:57 +0800

    Thread state demo

---
+++ thread/src/main/java/org/tanc/concurrent/thread/SleepUtils.java
@@ -0,0 +1,13 @@
+package org.tanc.concurrent.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class SleepUtils {
+    public static final void second(long second) {
+        try {
+            TimeUnit.SECONDS.sleep(second);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
