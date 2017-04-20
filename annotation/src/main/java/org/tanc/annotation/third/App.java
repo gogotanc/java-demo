018d3066762ffa77722157929c7e24e965376df7
gogotanc <gogotanc@163.com>
2017-04-20 20:24:47 +0800

    annotation demo

--- annotation/src/main/java/org/tanc/annotation/third/App.java
+++ annotation/src/main/java/org/tanc/annotation/third/App.java
@@ -3,7 +3,6 @@ package org.tanc.annotation.third;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
-
 /**
  * Spring Demo
  * Created by tanc on 2017/4/15.
@@ -13,13 +12,11 @@ import org.springframework.context.annotation.*;
 @PropertySource("app.properties")
 public class App {
 
-
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
 
         MainController controller = context.getBean(MainController.class);
         MainController controller1 = context.getBean(MainController.class);
-
 
         controller.run();
         controller1.run();
