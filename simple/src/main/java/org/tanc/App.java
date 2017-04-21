3bd3d20ffa59da579ea6264e11cfae96cde7a5fd
gogotanc <gogotanc@163.com>
2017-04-21 22:30:13 +0800

    show date

---
+++ simple/src/main/java/org/tanc/App.java
@@ -0,0 +1,42 @@
+package org.tanc;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+/**
+ * 日历
+ * Created by tanc on 2017/4/21.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+
+        date = date.minusDays(today - 1);
+
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();
+
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for (int i = 1; i < value; i++) {
+            System.out.print("    ");
+        }
+        while (date.getMonthValue() == month) {
+            System.out.printf("%3d", date.getDayOfMonth());
+            if (date.getDayOfMonth() == today) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+            date = date.plusDays(1);
+            if (date.getDayOfWeek().getValue() == 1) {
+                System.out.println();
+            }
+        }
+        if (date.getDayOfWeek().getValue() != 1) {
+            System.out.println();
+        }
+    }
+}
